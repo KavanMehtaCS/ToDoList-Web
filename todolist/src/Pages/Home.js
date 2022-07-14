e941ce0cfab947ce6d7ee703e64e3808c4e68c05
AjileVenkat <ajayvenkat2812@gmail.com>
2022-07-14 18:42:14 -0500

    Update Home.js

--- todolist/src/Pages/Home.js
+++ todolist/src/Pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react"
+import Popup from '../Components/Popup'
+import {useState} from 'react'
 
 export default function Home(){
-    return(
+    const [buttonPopup, setButtonPopup] = useState(false);
+    return (
+    <div>
         <h1>Home Page</h1>
+        <h1> Events </h1>
+        <button onClick={() => setButtonPopup(true)}> Add Event </button>
+        <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}>
+            <h3> My Popup </h3>
+        </Popup>
+    </div>
     )
 }
