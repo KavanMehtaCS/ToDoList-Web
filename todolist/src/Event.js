90fb6858fc2b5a7e55036db3aa3d138870ba39b0
AjileVenkat <ajayvenkat2812@gmail.com>
2022-07-14 10:42:00 -0500

    popup

--- todolist/src/Event.js
+++ todolist/src/Event.js
@@ -1,2 +1,14 @@
 import React from 'react';
+import Popup from './Components/Popup'
+import {useState} from 'react'
 
+export default function Event() {
+    const [buttonPopup, setButtonPopup] = useState(false);
+    <div>
+        <h1> Events </h1>
+        <button onClick={() => setButtonPopup(true)}> Add Event </button>
+        <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}>
+            <h3> My Popup </h3>
+        </Popup>
+    </div>
+}
