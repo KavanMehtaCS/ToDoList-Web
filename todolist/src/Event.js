cc422fc5b57087354bfe9bb48a751ec91205b24e
AjileVenkat <ajayvenkat2812@gmail.com>
2022-07-13 23:14:18 -0500

    Revert "event changes"
    This reverts commit 2dc8c899bb92b1985e9e22850172e0fecc968084.

--- todolist/src/Event.js
+++ todolist/src/Event.js
@@ -1,15 +1,2 @@
 import React from 'react';
-import Popup from './components/Popup'
-
-export default function Event() {
-    return (
-        <div>
-            <h1> Events </h1>
-            <button> Add New Event </button>
-            <Popup trigger = {true}> 
-                <h3> My Popup </h3>
-            </Popup>
-        </div>
-    );
- }
 
