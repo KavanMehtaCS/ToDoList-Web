775277452848ccc9771b86bab269a0fe4a29e0f3
SXV357 <shrevis2018@gmail.com>
2022-07-15 13:33:49 +0530

    Header for features tab and features data file

--- todolist/src/Pages/Features.js
+++ todolist/src/Pages/Features.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function Features(){
     return(
-        <h1>Features Page</h1>
+        <>
+            <h1>Staying on top of all your tasks</h1>
+            <p>Todolist gives you the ability to personalize your tasks and keep a track of deadlines 
+                so that you can maximize your productivity</p>
+        </>
     )
 }
